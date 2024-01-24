5b9df4c8864db5d0b4f845f33651215cb24b6387
Hasan <hasan.error.code@gmail.com>
2024-01-25 00:25:21 +0600

    Create app

--- StudyMart/settings.py
+++ StudyMart/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR/'Templates'
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Course',
 ]
 
 MIDDLEWARE = [
