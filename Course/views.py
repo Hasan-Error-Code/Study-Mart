5ec5a3f1e2d171814d9b4d222b4b255d179f8565
Hasan <hasan.error.code@gmail.com>
2024-01-28 17:17:44 +0600

    Variable

--- Course/views.py
+++ Course/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def Course(request):
     return render(request, 'Course/Course.html')
+def Name(request):
+    return render(request, 'Course/Course.html', {'Name': "Hasan"})
