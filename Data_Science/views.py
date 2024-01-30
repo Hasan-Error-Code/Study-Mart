7ce20e2cd62995be0d6ece1ef736a82afe223711
Hasan <hasan.error.code@gmail.com>
2024-01-30 20:06:10 +0600

    loop

--- Data_Science/views.py
+++ Data_Science/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def Home(request):
-    return render(request, 'Data_Science/Home.html')
 def About(request):
     return render(request, 'Data_Science/About.html')
+def Home(request):
+    Available_course = {'course': ["Machine Learning", "Data Science", "Django", "Deep Learning", "Data Mining"]}
+    return render(request, 'Data_Science/Home.html', Available_course)
